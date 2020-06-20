da7ef02d25216a0372cd859ecec5914e47c80a05
Christian Castro <alvescastro00cca13@gmail.com>
2020-06-20 18:34:09 -0300

    About page

---
+++ src/components/Profile/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+const Profile = () =>(
+    <div className="Profile-wrapper">
+        <h1>Christian Castro</h1>
+        <h2>Programador Full-Stack</h2>
+        <p>
+            Programado Full-Stack na empresa de desenvolvimento de software TecNorth
+        </p>
+    </div>
+)
+
+export default Profile 
