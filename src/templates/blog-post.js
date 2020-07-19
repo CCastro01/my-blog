7139240e330bcec83424f2cd8ee2726a21fc8041
Christian <alvescastro00cca13@gmail.com>
2020-07-19 18:34:08 -0300

    disqus-comments

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,6 +2,8 @@ import React from 'react'
 import {graphql} from 'gatsby'
 import Layout from '../components/Layout'
 import RecommendedPosts from '../components/RecommendedPosts'
+import Comments from "../components/Comments"
+
 import * as S from '../components/Post/styled'
 
 
@@ -24,6 +26,7 @@ const BlogPost = ({data , pageContext }) => {
                 <div dangerouslySetInnerHTML={{__html:post.html}}></div>
             </S.MainContent>
             <RecommendedPosts next={next} previous={previous} />
+            <Comments url={post.fields.slug} title={post.frontmatter.title}/>
         </Layout>
     )
 }
@@ -31,6 +34,9 @@ const BlogPost = ({data , pageContext }) => {
     export const query = graphql`
     query Post($slug: String!) {
         markdownRemark(fields:{slug: {eq:$slug}}) {
+            fields{
+                slug
+            }
             frontmatter {
                 title
                 description
