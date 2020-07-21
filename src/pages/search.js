878d5d020c2acb67f348fa20c2bd4661cadd16dc
Christian <alvescastro00cca13@gmail.com>
2020-07-21 01:27:07 -0300

    Creating Search page

---
+++ src/pages/search.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from "../components/Layout"
+import Search from "../components/Search"
+
+const SearchPage = () => (
+    <Layout>
+        <Search/>
+    </Layout>
+)
+export default SearchPage
