587ed731471d19ca3e4b8536496be81f946f8129
Christian <alvescastro00cca13@gmail.com>
2020-07-22 17:15:30 -0300

    finished dark/light mode

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect}from 'react'
 import * as S from './styled'
 
 import { Home } from "@styled-icons/boxicons-solid/Home"
@@ -7,8 +7,19 @@ import { UpArrowAlt as Arrow } from "@styled-icons/boxicons-regular/UpArrowAlt"
 import { LightBulb as Light } from "@styled-icons/octicons/LightBulb"
 import { Grid } from "@styled-icons/boxicons-solid/Grid"
 
-const MenuBar = () =>(
-    <S.MenuBarWrapper>
+const MenuBar = () =>{
+
+    const [theme, setTheme] = useState(null)
+
+    const isDarMode = theme === 'dark'
+
+    useEffect(() => {
+        setTheme(window.__theme)
+        window.__onThemeChange =() => setTheme(window.__theme)
+    }, [])
+
+    return(
+        <S.MenuBarWrapper>
         <S.MenuBarGroup>
 
             <S.MenuBarLink to="/" title="Voltar para Home">
@@ -26,7 +37,11 @@ const MenuBar = () =>(
         </S.MenuBarGroup>
 
         <S.MenuBarGroup>
-            <S.MenuBarItem title="Mudar o tema">
+            <S.MenuBarItem title="Mudar o tema" onClick={() => {
+                window.__setPreferredTheme(isDarMode ? 'light' : 'dark') 
+            }}
+            className={theme}
+            >
                 <Light/>
             </S.MenuBarItem>
             <S.MenuBarItem title="Mudar visualização">
@@ -37,6 +52,7 @@ const MenuBar = () =>(
             </S.MenuBarItem>
         </S.MenuBarGroup>
     </S.MenuBarWrapper>
-)
+    )
+}
 
 export default MenuBar
